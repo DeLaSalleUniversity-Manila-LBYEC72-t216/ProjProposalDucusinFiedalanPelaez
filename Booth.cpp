8ab9cc9a8fcb8eb90cd6d276f2f9d18ff397ebaf
Fieds <karlo_fiedalan@dlsu.edu.ph>
2017-08-06 17:48:09 +0800

    Add files via upload

---
+++ Booth.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <time.h>
+#include <string>
+
+using namespace std;
+
+typedef struct construction{
+	string plate;
+	int control;
+
+typedef struct calendar{
+	tm time
+};
+}data;
+
+int main() {
+
+int i,p,m;
+char n;
+data sheet[30];
+
+for(i=0;i<30;i++)
+{
+sheet.control[i] = 0;
+sheet.plate[i] = "n/a";
+}
+
+m=0;
+
+do
+{
+system("CLS");
+
+cout<<"	Ticketing Booth \n \n \n"
+	<<"1 - Add \n"
+	<<"2 - Search / Inquire \n"
+	<<"3 - Bill \n"
+	<<"4 - Exit \n \n";
+cin>>p;
+
+//--------------------------------------------------------------------------------------
+
+switch(p)
+{
+case 1:
+{
+system("CLS");
+cout<<"\n \n Input Plate number: ";
+cin>>sheet.plate[m];
+
+sheet.control[m]= 0816217;
+m++;
+
+cout<<"\n Database updated! \n \n"
+system("pause");
+
+n='Y';
+
+}break;
+
+
+case 2:
+{
+
+}break;
+
+
+case 3:
+{
+
+}break;
+
+
+case 4:
+{
+return 0;
+}break;
+
+
+default: cout<<"Invalid input \n \n";system("pause");n='Y';break;
+
+}
+	
+}while(n=='Y');
+	return 0;
+}
